b4031f378017d5f82911e49c929cc20be3beb243
Maniteja <135437049+Maniteja0126@users.noreply.github.com>
2024-03-02 14:21:02 +0530

    Time: 87 ms (52.50%) | Memory: 56.4 MB (36.94%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.js
@@ -3,5 +3,13 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-     return  nums.map((num)=>num*num).sort((a,b)=>a-b)
+    //  return  nums.map((num)=>num*num).sort((a,b)=>a-b)
+    for (let i = 0; i < nums.length; i++) {
+        nums[i] = nums[i] * nums[i];
+    }
+    
+    
+    nums.sort((a, b) => a - b);
+    
+    return nums;
 };
