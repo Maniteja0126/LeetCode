d24ae46453838a7d50ceb41bc1203d9b5d1f068d
Maniteja <135437049+Maniteja0126@users.noreply.github.com>
2024-04-02 20:57:59 +0530

    Time: 8 ms (78.67%) | Memory: 42.5 MB (37.68%) - LeetSync

---
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int[] freqS = new int[127];
+     int[] freqT = new int[127];
+
+     for(int i=0;i<s.length();i++){
+        if(freqS[s.charAt(i)] != freqT[t.charAt(i)]) return false;
+        freqS[s.charAt(i)] = i+1;
+        freqT[t.charAt(i)] = i+1;
+     }   
+     return true;
+    }
+}
